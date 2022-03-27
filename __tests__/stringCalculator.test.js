aad6d23369001c05d2e3519ce8e49721407af883
Shubham Dhore <shubhamvdhore@gmail.com>
2022-03-27 15:53:34 +0530

    Changes to handle multiple comma separated values

--- __tests__/stringCalculator.test.js
+++ __tests__/stringCalculator.test.js
@@ -4,4 +4,5 @@ const add = require("../stringCalculator");
 test('Should return number of 0 for empty String', () => {
     expect(add("")).toBe(0);
     expect(add("5")).toBe(5);
+    expect(add("1,2,3,4,5")).toBe(15);
 });
--- stringCalculator.js
+++ stringCalculator.js
@@ -1,5 +1,18 @@
+
 const add  = function(exp){
-    return parseInt(exp) || 0;
+    let sum = 0;
+    let nums = exp.split(",");
+    sum = getSumOfStringArray(nums);
+    return sum;
 };
+
+function getSumOfStringArray(nums){
+    let sum = 0;
+    for(let i = 0; i < nums.length; i++){
+        sum += parseInt(nums[i]) || 0;
+    }
+
+    return sum;
+}
 
 module.exports = add;
